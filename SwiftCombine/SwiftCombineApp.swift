9e95d17838d113b9eb17db474ae716e92f3d2234
ehsan2793 <ehsan2793@gmail.com>
2024-05-19 18:11:03 -0700

    Initial Commit

---
+++ SwiftCombine/SwiftCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCombineApp.swift
+//  SwiftCombine
+//
+//  Created by Ehsan Rahimi on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

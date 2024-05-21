779ec76f8aa840f06f4476c0cdcba84b8536d583
ehsan2793 <ehsan2793@gmail.com>
2024-05-20 18:36:40 -0700

    added background

--- SwiftCombine/ContentView.swift
+++ SwiftCombine/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ZStack(alignment: .top) {
+                Text("Hello, world!")
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(AccountBackground())
+            .toolbar(.hidden)
+            .padding()
+        } //: NAVIGATION
     }
 }
 

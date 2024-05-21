efe64d4ecdf7a583f4523761b72b77d45e0cf8a8
ehsan2793 <ehsan2793@gmail.com>
2024-05-20 19:14:51 -0700

    added tracking scroll

--- SwiftCombine/ContentView.swift
+++ SwiftCombine/ContentView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var contentOffset = CGFloat(0)
     var body: some View {
         NavigationStack {
             ZStack(alignment: .top) {
-                Text("Hello, world!")
-            }
+                TrackableScrollView(offsetChanged: {
+                    offset in
+                    contentOffset = offset.y
+                }) {
+                    Text("Hello, world!")
+                        .padding(.top, 40)
+                }
+
+                VisualEffectBlur(blurStyle: .systemMaterial)
+                    .opacity(contentOffset < -16 ? 1 : 0)
+                    .animation(.easeIn, value: contentOffset)
+                    .ignoresSafeArea()
+                    .frame(width: 600,height: 40)
+             
+            } //: zstack
             .frame(maxHeight: .infinity, alignment: .top)
             .background(AccountBackground())
             .toolbar(.hidden)
             .padding()
+            .ignoresSafeArea()
         } //: NAVIGATION
     }
 }

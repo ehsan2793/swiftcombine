5b689f31b16535698363551eb779daa79189c6e6
ehsan2793 <ehsan2793@gmail.com>
2024-05-22 20:41:42 -0700

    added menu row

--- SwiftCombine/ContentView.swift
+++ SwiftCombine/ContentView.swift
@@ -16,23 +16,74 @@ struct ContentView: View {
                     offset in
                     contentOffset = offset.y
                 }) {
-                    Text("Hello, world!")
+                    content
                         .padding(.top, 40)
                 }
 
-                VisualEffectBlur(blurStyle: .systemMaterial)
-                    .opacity(contentOffset < -16 ? 1 : 0)
-                    .animation(.easeIn, value: contentOffset)
-                    .ignoresSafeArea()
-                    .frame(width: 600,height: 40)
-             
+//                VisualEffectBlur(blurStyle: .systemUltraThinMaterialLight)
+//                    .opacity(contentOffset < -16 ? 1 : 0)
+//                    .animation(.easeIn, value: contentOffset)
+//                    .ignoresSafeArea()
+//
+//                    .frame(width: 600,height: 40)
             } //: zstack
-            .frame(maxHeight: .infinity, alignment: .top)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+
             .background(AccountBackground())
             .toolbar(.hidden)
+
             .padding()
             .ignoresSafeArea()
         } //: NAVIGATION
+    }
+
+    var content: some View {
+        VStack {
+            VStack {
+                NavigationLink {
+                    FAQView()
+                } label: {
+                    MenuRow()
+                }
+                divider
+                NavigationLink {
+                    PackagesView()
+                } label: {
+                    MenuRow(title: "Packages", leftIcon: "square.stack.3d.up.fill")
+                }
+
+                divider
+                Link(destination: URL(string: "https://www.apple.com")!, label: {
+                    MenuRow(title: "YouTube Channel", leftIcon: "play.rectangle.fill", rightIcon: "link")
+                })
+            }
+            .padding()
+            .background(Color(.background1))
+            .background(VisualEffectBlur(blurStyle: .systemMaterialDark))
+            .overlay {
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.white, lineWidth: 1)
+                    .blendMode(.overlay)
+            }
+            .mask(RoundedRectangle(cornerRadius: 20))
+
+            Text("Version 1.00")
+                .foregroundStyle(.white.opacity(0.7))
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .padding(.vertical, 20)
+        }
+        .foregroundStyle(.white)
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .padding(.bottom, 10)
+
+//        .background(Color.sy)
+    } //: content
+
+    var divider: some View {
+        Divider()
+            .background(Color.white.blendMode(.overlay))
     }
 }
 
---
+++ SwiftCombine/Views/PackagesView.swift
@@ -0,0 +1,18 @@
+//
+//  PackagesView.swift
+//  SwiftCombine
+//
+//  Created by Ehsan Rahimi on 5/22/24.
+//
+
+import SwiftUI
+
+struct PackagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PackagesView()
+}
